3326d889d00143096620b56b4d154a534c15e630
ismaelxyz <ismaelbeli.com@gmail.com>
2024-05-06 15:48:58 -0400

    doc: added readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# Fabricjs Vanilla
+
+This is a simple example of how to use Fabric Canvas in which I group elements together to create nice widgets, this can be used as inspiration or as a supporting element for bigger goals. 
+
+![Preview](show/Screenshot%202024-05-06%20153012.png)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone the repository.
+2. Install the dependencies using `npm install`.
+3. Run the project using `npm run build`.
+
+## Usage
+
+1. Open the `src/index.js` file.
+2. Modify the code according to your needs.
+3. Run the project using `npm run start`.
+
+## Contributing
+
+You can contribute by sending a PR or opening an issue or simply by giving a star to the project, this project is open to any contribution.
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).
--- src/index.js
+++ src/index.js
@@ -1,57 +1,122 @@
 import * as fabric from "fabric";
+import imageRaw from "./alarm.png";
 
-const CANVAS = new fabric.Canvas("whiteboard");
-
-
-const rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    width: 100,
-    height: 100,
-    fill: "coral"
+const CANVAS = new fabric.Canvas("whiteboard", {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
 });
 
-const textContainer = new fabric.Rect({
-	left: rect.left,
-	top: rect.top - 30,
-	fill: 'black',
-    borderColor: 'black',
-	width: rect.width,
-	height: 20,
-	rx: 1,
-	ry: 1.5,
-	angle: 0,
-	hasControls: true
+function generateLineGroup() {
+    const line1 = new fabric.Line([0, CANVAS.height / 2, CANVAS.width / 5, CANVAS.height / 2], {
+        stroke: 'black',
+        strokeWidth: 5,
+    });
+
+    const line2 = new fabric.Line([CANVAS.width / 5, CANVAS.height / 2, (2 * CANVAS.width) / 5, CANVAS.height / 2], {
+        stroke: 'gray',
+        strokeWidth: 5,
+    });
+
+    const line3 = new fabric.Line([(2 * CANVAS.width) / 5, CANVAS.height / 2, (3 * CANVAS.width) / 5, CANVAS.height / 2], {
+        stroke: 'lightgray',
+        strokeWidth: 5,
+    });
+
+    const line4 = new fabric.Line([(3 * CANVAS.width) / 5, CANVAS.height / 2, (4 * CANVAS.width) / 5, CANVAS.height / 2], {
+        stroke: 'whitesmoke',
+        strokeWidth: 5,
+    });
+
+    const line5 = new fabric.Line([(4 * CANVAS.width) / 5, CANVAS.height / 2, CANVAS.width, CANVAS.height / 2], {
+        stroke: 'white',
+        strokeWidth: 5,
+    });
+
+    return new fabric.Group([line1, line2, line3, line4, line5], {
+        left: 0,
+        top: CANVAS.height / 2,
+        transparentCorners: false,
+        hasControls: false
+    });
+}
+
+async function generateImageGroup() {
+    const rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        width: 80,
+        height: 80,
+        rx: 15,
+        ry: 15,
+        fill: "white",
+        shadow: 'rgba(0,5,0,0.9) 0px 5px 7px'
+    });
+
+
+    const image = await fabric.FabricImage.fromURL(imageRaw);
+    image.scale(0.1);
+    image.set({
+        left: rect.left + rect.width / 2,
+        top: rect.top + rect.height / 2,
+        originX: "center",
+        originY: "center",
+        fill: "blue",
+    });
+
+    const textContainer = new fabric.Rect({
+        left: rect.left,
+        top: rect.top - 30,
+        fill: 'black',
+        borderColor: 'black',
+        width: rect.width,
+        height: 20,
+        rx: 1,
+        ry: 1.5,
+        angle: 0,
+        hasControls: true
+    });
+
+    const triangle = new fabric.Triangle({
+        left: textContainer.left + textContainer.width / 2,
+        top: textContainer.top + textContainer.height + 5,
+        width: 10,
+        height: 8,
+        fill: 'black',
+        angle: 180,
+        originX: "center",
+        originY: "center",
+        borderColor: 'transparent',
+    });
+
+
+    const text = new fabric.FabricText("Name", {
+        fontFamily: "Arial",
+        fill: "white",
+        fontSize: 10,
+        left: textContainer.left + textContainer.width / 2,
+        top: textContainer.top + textContainer.height / 2,
+        originX: "center",
+        originY: "center"
+    });
+
+
+
+    return new fabric.Group([textContainer, triangle, text, rect, image], {
+        left: 0,
+        top: 0,
+        transparentCorners: false,
+        hasControls: false
+    });
+
+}
+
+async function main() {
+    let widgets = [await generateImageGroup(), generateLineGroup()];
+    widgets.forEach(widget => CANVAS.add(widget));
+}
+
+
+main().then(() => {
+    console.log("Done");
+}).catch((error) => {
+    console.error(error);
 });
-
-const triangle = new fabric.Triangle({
-    left: textContainer.left + textContainer.width / 2,
-    top: textContainer.top + textContainer.height + 5,
-    width: 10,
-    height: 8,
-    fill: 'black',
-    angle: 180,
-    originX: "center",
-    originY: "center",
-    borderColor: 'transparent',
-});
-
-
-let text = new fabric.FabricText("Name", {
-    fontFamily: "Arial",
-    fill: "white",
-    fontSize: 10,
-    left: textContainer.left + textContainer.width / 2,
-    top: textContainer.top + textContainer.height / 2,
-    originX: "center",
-    originY: "center"
-});
-
-let group = new fabric.Group([textContainer, triangle, rect, text], {
-    left: 0,
-    top: 0,
-    transparentCorners: false,
-    hasControls: false
-});
-
-CANVAS.add(group);
